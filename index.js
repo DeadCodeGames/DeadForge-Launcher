cf61217af5962dc5f3ded077333a50e7df9a37de
Richard Kužela <richard@kanshen.click>
2024-03-01 10:00:30 +0100

    Version v1.0.0
    v1.0.0 Release Candidate A
    - Fixed autoupdate not working on Linux and possibly Mac
    - Extended window controls to the corner
    - Added Color indicators for window controls on hover
    - Added Library Button and Library Section
    
    -------------------------
    
    v1.0.0 Release Candidate B
    - Vertically centered the Minimize Button
    - Fixed force quitting without confirming update dialog not launching the updater
    
    -------------------------
    
    v1.0.0 Release Candidate C
    - Changed the Credits section to show credits in the form of pills
    
    -------------------------
    
    v1.0.0 Release Candidate D
    - Fixed auto-updater downloading already downloaded update
    
    -------------------------
    
    v1.0.0 Release Candidate E
    - Added Beta Version Switch
    - Fixed preferences file being erased upon update (will take effect after this update)
    
    -------------------------
    
    v1.0.0 Release Candidate F
    - Fixed downloading update over another and corrupting the update file
    - Fixed Library Section not having Light Mode

--- index.js
+++ index.js
@@ -25,8 +25,7 @@ closeBtn.addEventListener('click', () => {
 });
 
 ipcRenderer.on('updateStatus', (event, statusobject) => {
-  const { status, current, latest, failType } = statusobject;
-  console.log(status); console.log(current); console.log(latest); console.log(failType);
+  const { status, current, latest, failType, betaEnabled, pendingBetaUpdates } = statusobject;
   const statusElement = document.querySelector('div#updatestatus');
   const settingsStatusElement = document.querySelector('section#settings div#updatestatusSETTINGS');
   const statusString = document.querySelector('div#updatestatusSETTINGSstring');
@@ -34,12 +33,14 @@ ipcRenderer.on('updateStatus', (event, statusobject) => {
   settingsStatusElement.setAttribute('status', status);
   if (status == 'checking') {
     statusString.textContent = 'Checking for updates...';
-  } else if (status == 'uptodate') {
+  } else if (status == 'uptodate' && betaEnabled) {
     statusString.textContent = 'You are running the latest version: ' + current;
+  } else if (status == 'uptodate' && !betaEnabled) {
+    statusString.textContent = 'There are no newer stable updates. You are running version ' + current + '. There ' + (pendingBetaUpdates == 1 ? 'is' : 'are') + ' ' + (pendingBetaUpdates == 0 ? 'no' : pendingBetaUpdates) + ' beta update' + (pendingBetaUpdates == 1 ? '' : 's') + ' available.';
   } else if (status == 'downloading') {
-    statusString.textContent = 'Downloading update... ' + current + ' -> ' + latest;
+    statusString.textContent = 'Downloading latest update... ' + current + ' -> ' + latest;
   } else if (status == 'downloaded') {
-    statusString.textContent = 'Downloaded update: ' + latest;
+    statusString.textContent = 'Downloaded latest update: ' + latest;
   } else if (status == 'fail' && failType == 'check') {
     statusString.textContent = 'Failed to check for updates. Are you connected to the internet?';
   } else if (status == 'fail' && failType == 'download') {
@@ -64,6 +65,7 @@ function sendColorPreference() {
 
 const discordRichPresenceSwitch = document.querySelector('input#DiscordRPCSwitch');
 const startupSwitch = document.querySelector('input#StartupSwitch');
+const betaSwitch = document.querySelector('input#BetaSwitch');
 
 ipcRenderer.on('preferences', (event, preferencesData) => {
   const preferences = JSON.parse(preferencesData);
@@ -71,6 +73,7 @@ ipcRenderer.on('preferences', (event, preferencesData) => {
   document.querySelector('html').classList.add(preferences.colorScheme);
   discordRichPresenceSwitch.checked = preferences.discordRPC;
   if (process.platform !== 'linux') { startupSwitch.checked = preferences.startup } else { startupSwitch.disabled = true };
+  betaSwitch.checked = preferences.betaEnabled;
 });
 
 function checkForUpdates() {
@@ -86,3 +89,13 @@ startupSwitch.addEventListener('change', (event) => {
   const isChecked = event.target.checked;
   ipcRenderer.send('toggleRunOnStartup', isChecked);
 });
+
+betaSwitch.addEventListener('change', (event) => {
+  const isChecked = event.target.checked;
+  ipcRenderer.send('toggleBeta', isChecked);
+});
+
+ipcRenderer.on('disableUpdateButton', (event, status) => {
+  betaSwitch.disabled = status;
+  status == true ? betaSwitch.setAttribute('temp', status) : betaSwitch.removeAttribute('temp');
+})
--- main.js
+++ main.js
@@ -3,13 +3,14 @@ require('@electron/remote/main').initialize()
 const DiscordRPC = require('discord-rpc');
 const path = require('node:path');
 const axios = require('axios');
-const fs = require('fs');;
+const fs = require('fs');
+const downloadsFolder = require('downloads-folder');
 
-let mainWindow, updateStatus = (statusobject) => { mainWindow.webContents.send('updateStatus', statusobject); }, changeColorMode = (color) => { mainWindow.webContents.send('changeColorMode', color); }
+let mainWindow, updateStatus = (statusobject) => { mainWindow.webContents.send('updateStatus', statusobject); }, changeColorMode = (color) => { mainWindow.webContents.send('changeColorMode', color); }, preferences;
 
 const createWindow = () => {
   mainWindow = new BrowserWindow({
-    minWidth: 500,
+    minWidth: 555,
     minHeight: 350,
     height: 600,
     width: 800,
@@ -20,6 +21,7 @@ const createWindow = () => {
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
+      devTools: false
     }
   });
 
@@ -39,6 +41,17 @@ const createWindow = () => {
 app.whenReady().then(() => {
   createWindow()
   setTimeout(() => {
+    if (fs.existsSync(path.join(downloadsFolder(), 'deadforge.preferences.json'))) {
+      const preferencesBackupData = fs.readFileSync(path.join(downloadsFolder(), 'deadforge.preferences.json'), 'utf8');
+      const preferencesBackup = JSON.parse(preferencesBackupData);
+
+      const requiredKeys = ['colorScheme', 'discordRPC', 'startup', 'betaEnabled'];
+      const missingKeys = requiredKeys.filter(key => !(key in preferencesBackup));
+
+      if (missingKeys.length === 0) {
+        fs.renameSync(path.join(downloadsFolder(), 'deadforge.preferences.json'), path.join(path.join(path.dirname(__dirname), 'app.asar.unpacked'), 'preferences.json'));
+      }
+    }
     fs.readFile(path.join(__dirname, 'preferences.json'), 'utf8', (err, data) => {
       if (err) {
           console.error('Error reading preferences file:', err);
@@ -46,6 +59,7 @@ app.whenReady().then(() => {
       }
 
       mainWindow.webContents.send('preferences', data);
+      preferences = JSON.parse(data);
       JSON.parse(data).discordRPC ? connectRPC() : null;
   });
     checkUpdates()
@@ -109,14 +123,29 @@ function connectRPC() {
   });
 }
 
+function disableUpdateButton(status) {
+  mainWindow.webContents.send('disableUpdateButton', status);
+}
+
 async function checkUpdates() {
+  disableUpdateButton(true);
   updateStatus({"status": "checking", "current": version});
-  var JS = await fetch("https://api.github.com/repos/DeadCodeGames/DeadForge/releases").then(response => response.json()).catch(err => { updateStatus({"status": "fail", "current": version, "latest": undefined, "failType": "check"}); console.error(err) }), assets, downloadLinksByOS = {}, platform, latestversion, installerPath;
+  var JS = await fetch("https://api.github.com/repos/DeadCodeGames/DeadForge/releases").then(response => response.json()).catch(err => { updateStatus({ "status": "fail", "current": version, "latest": undefined, "failType": "check" }); console.error(err) }), assets, downloadLinksByOS = {}, platform, latestversion, installerPath, currentlydownloadedupdate, updateIndex = 0, pendingBetaUpdates = 0;
+  var betaEnabled = JSON.parse(fs.readFileSync(path.join(__dirname, 'preferences.json'), 'utf8')).betaEnabled;
+  if (!Array.isArray(JS)) { updateStatus({ "status": "fail", "current": version, "latest": undefined, "failType": "check" }); disableUpdateButton(false); return }
 
-  assets = JS[0].assets;
-  latestversion = JS[0].tag_name;
+  for (updateIndex; updateIndex < JS.length; updateIndex++) {
+    if (JS[updateIndex].tag_name == version) { break }
+    if ((JS[updateIndex].prerelease == true && !betaEnabled)) { pendingBetaUpdates++; continue } else { break }
+  }
 
-  if (latestversion == version) {updateStatus({"status": "uptodate", "current": version}); return};
+  if (updateIndex == JS.length) { updateStatus({ "status": "uptodate", "current": version, "latest": latestversion, failType: null, betaEnabled: betaEnabled, pendingBetaUpdates: pendingBetaUpdates }); disableUpdateButton(false); return }
+  
+  assets = JS[updateIndex].assets;
+  latestversion = JS[updateIndex].tag_name;
+
+  if (latestversion == version) { updateStatus({ "status": "uptodate", "current": version, "latest": latestversion, failType: null, betaEnabled: betaEnabled, pendingBetaUpdates: pendingBetaUpdates }); disableUpdateButton(false); return }
+  else if (latestversion == currentlydownloadedupdate) { updateStatus({ "status": "downloaded", "current": version, "latest": latestversion }); return };
 
   assets.forEach(asset => {
     const fileName = asset.name.toLowerCase();
@@ -147,14 +176,14 @@ async function checkUpdates() {
       cancelId: 1
     };
   
-    dialog.showMessageBox(null, options).then((result) => { if (result.response == 0) { app.quit(); shell.openExternal(installerPath); } else { app.on('before-quit', () => { app.quit(); shell.openExternal(installerPath);});}});
+    dialog.showMessageBox(null, options).then((result) => { if (result.response == 0) { app.quit(); shell.openExternal(installerPath); }});
   }
 
   async function downloadUpdate() {
     var writer;
-    if (platform == "windows") { installerPath = path.join(path.dirname(path.dirname(path.dirname(__dirname))), 'update.exe'); writer = fs.createWriteStream(installerPath); }
-    else if (platform == "mac") { installerPath = path.join(path.dirname(path.dirname(path.dirname(__dirname))), 'update.dmg'); writer = fs.createWriteStream(installerPath); }
-    else if (platform == "linux") { installerPath = path.join(path.dirname(path.dirname(path.dirname(__dirname))), 'update.deb'); writer = fs.createWriteStream(installerPath); }
+    if (platform == "windows") { installerPath = path.join(downloadsFolder(), 'update.exe'); writer = fs.createWriteStream(installerPath); }
+    else if (platform == "mac") { installerPath = path.join(downloadsFolder(), 'update.dmg'); writer = fs.createWriteStream(installerPath); }
+    else if (platform == "linux") { installerPath = path.join(downloadsFolder(), 'update.deb'); writer = fs.createWriteStream(installerPath); }
 
     updateStatus({"status": "downloading", "current": version, "latest": latestversion});
   
@@ -172,45 +201,46 @@ async function checkUpdates() {
     });
   }
 
-  await downloadUpdate().then(() => {updateStatus({'status': 'downloaded', 'current': version, 'latest': latestversion}); showInstallDialog()}).catch(err => { updateStatus({"status": "fail", "current": version, "latest": latestversion, "failType": "download"}); console.error(err) });
+  await downloadUpdate().then(() => { disableUpdateButton(false); updateStatus({ 'status': 'downloaded', 'current': version, 'latest': latestversion }); { app.on('before-quit', () => { fs.renameSync(path.join(path.join(path.dirname(__dirname), 'app.asar.unpacked'), 'preferences.json'), path.join(downloadsFolder(), 'deadforge.preferences.json')); shell.openExternal(installerPath); }); };  showInstallDialog()}).catch(err => { disableUpdateButton(false); updateStatus({"status": "fail", "current": version, "latest": latestversion, "failType": "download"}); console.error(err) });
 }
 
+  ipcMain.on('color-preference', (event, colorPreference) => {
+    preferences.colorScheme = colorPreference;
+    writePreferences()
+  });
 
-var preference = {
-  colorScheme: 'dark',
-  discordRPC: true,
-  startup: false
-};
+  ipcMain.on('toggleDiscordRichPresence', (event, discordPreference) => {
+    preferences.discordRPC = discordPreference;
+    discordPreference == false ? rpc.destroy() : connectRPC();
+    writePreferences()
+  });
 
-ipcMain.on('color-preference', (event, colorPreference) => {
-  preference.colorScheme = colorPreference;
-  writePreferences()
-});
+  ipcMain.on('toggleRunOnStartup', (event, startupPreference) => {
+    if (process.platform == 'linux') return;
+    preferences.startup = startupPreference;
+    startupPreference == false ? app.setLoginItemSettings({ openAtLogin: false }) : app.setLoginItemSettings({ openAtLogin: true });
+    writePreferences()
+  });
 
-ipcMain.on('toggleDiscordRichPresence', (event, discordPreference) => {
-  preference.discordRPC = discordPreference;
-  discordPreference == false ? rpc.destroy() : connectRPC();
-  writePreferences()
-});
-
-ipcMain.on('toggleRunOnStartup', (event, startupPreference) => {
-  if (process.platform == 'linux') return;
-  preference.startup = startupPreference;
-  startupPreference == false ? app.setLoginItemSettings({openAtLogin: false}) : app.setLoginItemSettings({openAtLogin: true});
-  writePreferences()
-});
-
-function writePreferences() {
-  const jsonData = JSON.stringify(preference, null, 2);
-
-  fs.writeFile(path.join(__dirname, 'preferences.json'), jsonData, 'utf8', (err) => {
-    if (err) {
-      console.error('preferences', err);
-      return;
+  ipcMain.on('toggleBeta', (event, betaPreference) => {
+    preferences.betaEnabled = betaPreference;
+    if (betaPreference == true) {
+      checkUpdates();
     }
-  }); 
-}
+    writePreferences()
+  })
 
-ipcMain.on('update-check', (event) => {
-  checkUpdates();
-})
+  function writePreferences() {
+    const jsonData = JSON.stringify(preferences, null, 2);
+
+    fs.writeFile(path.join(__dirname, 'preferences.json'), jsonData, 'utf8', (err) => {
+      if (err) {
+        console.error('preferences', err);
+        return;
+      }
+    });
+  }
+
+  ipcMain.on('update-check', (event) => {
+    checkUpdates();
+  })
